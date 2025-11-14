c21d251a82e4489b172d7c258d574ed0d6d92774
jamesy-tech <jamesycreator@gmail.com>
2025-11-14 15:33:57 +1300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector('.commandInputT5');
 
 const data = {
   name: "BookUI+",
-  version: "V1.9"
+  version: "V2.0"
 };
 document.title = `${data.name} ${data.version}`;
 document.getElementById("nameElementR4").textContent = `${data.name} ${data.version}`;
