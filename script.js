10ff649b1ef85a3072328aaee130b70822509493
jamesy-tech <jamesycreator@gmail.com>
2026-03-21 19:38:32 +1300

    Update script.js

--- script.js
+++ script.js
@@ -15,21 +15,30 @@ document.getElementById("getBookUIBtn").style.display = "none";
 
 async function updateVersionFromGithub() {
   try {
-    const response = await fetch(
-      "https://raw.githubusercontent.com/Jamesy-tech/BookUI/main/version.json"
-    );
+    const repo = "Jamesy-tech/BookUI";
 
-    const json = await response.json();
+    const response = await fetch(`https://api.github.com/repos/${repo}/commits?per_page=1`);
+    
+    const linkHeader = response.headers.get('Link');
+    if (linkHeader) {
 
-    data.version = `V${json.version}`;
+      console.log(linkHeader);
 
-    const nameElem = document.getElementById("nameElementR4");
-    if (nameElem) {
-      nameElem.textContent = `${data.name} ${data.version}`;
+      const match = linkHeader.match(/page=(\d+)>; rel="last"/);
+      if (match) {
+        const totalCommits = parseInt(match[1]);
+
+        const formattedVersion = (totalCommits * 0.1).toFixed(1);
+        data.version = `V${formattedVersion}`;
+        
+        const nameElem = document.getElementById("nameElementR4");
+        if (nameElem) {
+          nameElem.textContent = `${data.name} ${data.version}`;
+        }
+      }
     }
-
   } catch (err) {
-    console.error("Failed to fetch version", err);
+    console.error("Failed to fetch version, keeping V4.1", err);
   }
 }
 
